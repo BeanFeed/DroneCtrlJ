ddf0544de6cc3260b6be674cb7f092f0a4e14446
CrackedLua <86930720+CrackedLua@users.noreply.github.com>
2022-03-21 15:09:54 -0400

    Commit

--- Python/bridge.py
+++ Python/bridge.py
@@ -6,6 +6,7 @@ from djitellopy import Tello
 message = " "
 drone = Tello()
 drone.connect()
+drone.takeoff()
 
 def runCMD(cmd):
     if(type(cmd) is str):
@@ -16,6 +17,24 @@ def runCMD(cmd):
             a3 = int(cmd[3])
             a4 = int(cmd[4])
             drone.send_rc_control(a1,a2,a3,a4)
+        elif(cmd == "Takeoff"):
+            pass
+            #drone.takeoff()
+        elif(cmd == "Land"):
+            drone.land()
+            pass
+        elif(cmd.startswith("flip")):
+            cmd = cmd.split()
+            dir = cmd[1]
+            if(dir == "f"):
+                drone.flip_forward()
+            elif(dir == "l"):
+                drone.flip_left()
+            elif(dir == "b"):
+                drone.flip_back()
+            elif(dir == "r"):
+                drone.flip_right()
+
 async def handler(websocket):
     global message
     while True:
