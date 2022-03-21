fc4ac01b62d10f32abc2f23d1d87aee190761c6b
CrackedLua <two.austin.two@gmail.com>
2022-03-21 12:22:02 -0400

    added logic for rc control

--- Python/bridge.py
+++ Python/bridge.py
@@ -4,12 +4,18 @@ import websockets
 
 from djitellopy import Tello
 message = " "
-#drone = Tello()
-#drone.connect()
+drone = Tello()
+drone.connect()
 
 def runCMD(cmd):
-    pass
-    
+    if(type(cmd) is str):
+        if(cmd.startswith("rc")):
+            cmd = cmd.split()
+            a1 = int(cmd[1])
+            a2 = int(cmd[2])
+            a3 = int(cmd[3])
+            a4 = int(cmd[4])
+            drone.send_rc_control(a1,a2,a3,a4)
 async def handler(websocket):
     global message
     while True:
